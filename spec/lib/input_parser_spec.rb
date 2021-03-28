fca47d7db76dcb22aa46dd8d92cf035c23dde49d
alliesground <sameer.libang@gmail.com>
2021-03-29 00:06:23 +1100

    passing InputParse#parse test

---
+++ spec/lib/input_parser_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../../lib/input_parser'
+
+RSpec.describe InputParser do
+  describe '#parse' do
+    context 'with valid command' do
+      it 'returns hash of params' do 
+        input = "CREATE EVENT a_valid_event"
+        parsed_input = InputParser.new(input).parse
+
+        expect(parsed_input[:cmd]).to eq 'CREATE EVENT'
+        expect(parsed_input[:params][:event_name]).to eq 'a_valid_event'
+      end
+    end
+
+    context 'with invalid command' do
+      it 'returns nil' do
+        input = "CREATE EVENT an invalid event"
+        parsed_input = InputParser.new(input).parse
+
+        expect(parsed_input).to eq nil
+      end
+    end
+  end
+end
